a5f288d7ac318ea8e1e490b598fa7e02f168a6ac
Ashadul Islam <ashadulislam2003@gmail.com>
2026-03-07 21:05:34 +0600

    learn to create 3 style submenu, combain submenu, separete sub menu, submenu as parent menu

--- wp-content/plugins/learnplugin/learnplugin.php
+++ wp-content/plugins/learnplugin/learnplugin.php
@@ -33,8 +33,35 @@ function learn_plugin_menu()
         'dashicons-wordpress', // optional
         11 // optional
     );
+
+
+    // =========================================================== //
+
+    // submenu for main menu page
+    add_submenu_page(
+        'learn-plugin',
+        'Learn Plugin Submenu Page',
+        'Add New',
+        'manage_options',
+        'learn-plugin',
+        'learn_plugin_page'
+    );
+
+    // add submenu page in admin sidebar
+    add_submenu_page(
+        'learn-plugin',
+        'Learn Plugin Submenu Page',
+        'Learn Plugin Submenu Combined',
+        'manage_options',
+        'learn-plugin-submenu-combined',
+        'learn_plugin_submenu_page'
+    );
 }
 
+
+
+
+// =========================================================== //
 // function for display content in menu page
 function learn_plugin_page()
 {
@@ -42,6 +69,35 @@ function learn_plugin_page()
     <div class="wrap">
         <h1>Learn Plugin</h1>
         <p>This is the content of the Learn Plugin page.</p>
+    </div>
+    <?php
+}
+
+
+
+// add submenu page in admin sidebar / separate action hook for submenu
+add_action('admin_menu', 'learn_plugin_submenu');
+
+// function for add submenu in admin sidebar
+function learn_plugin_submenu()
+{
+    add_submenu_page(
+        'learn-plugin',
+        'Learn Plugin Submenu',
+        'Learn Plugin Submenu Separate',
+        'manage_options',
+        'learn-plugin-submenu-separate',
+        'learn_plugin_submenu_page'
+    );
+}
+
+// function for display content in submenu page
+function learn_plugin_submenu_page()
+{
+    ?>
+    <div class="wrap">
+        <h1>Learn Plugin Submenu</h1>
+        <p>This is the content of the Learn Plugin Submenu page.</p>
     </div>
     <?php
 }
