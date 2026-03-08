92b5c7c46ca1460b5c58f8b9b750f15a7731b290
Ashadul Islam <ashadulislam2003@gmail.com>
2026-03-08 23:51:36 +0600

    learn to delete custom  page while deactivation / delete plugin

--- wp-content/plugins/learnplugin/example-code/delete-page-on-deactivation-example.php
+++ wp-content/plugins/learnplugin/example-code/delete-page-on-deactivation-example.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // create custom page while plugin activation
 function learn_plugin_create_custom_page()
 {
@@ -21,3 +20,18 @@ function learn_plugin_create_custom_page()
 }
 
 register_activation_hook(__FILE__, 'learn_plugin_create_custom_page');
+
+
+
+// delete the page created by the plugin when the plugin is deactivated
+function learn_plugin_delete_custom_page()
+{
+    $page_id = get_option('learn_plugin_page_id'); // get the page id from the database
+    if ($page_id) {
+        wp_delete_post($page_id, true); // delete the page permanently
+        delete_option('learn_plugin_page_id'); // delete the option from the database
+    }
+}
+
+register_deactivation_hook(__FILE__, 'learn_plugin_delete_custom_page'); // delete the custom page on deactivation
+// register_uninstall_hook(__FILE__, 'learn_plugin_delete_custom_page'); // delete the custom page on uninstallation
--- wp-content/plugins/learnplugin/learnplugin.php
+++ wp-content/plugins/learnplugin/learnplugin.php
@@ -134,13 +134,12 @@ register_deactivation_hook(__FILE__, 'learn_plugin_uninstall');
 // load_plugin_textdomain('learn-plugin', false, basename(dirname(__FILE__)) . '/languages/');
 
 
-// create custom post type while plugin activation
-function learn_plugin_create_custom_post_type()
+// create custom page while plugin activation
+function learn_plugin_create_custom_page()
 {
     $page = [];
-    // $page['post_type'] = 'learn_plugin_post_type';
-    $page['post_title'] = __('Learn Plugin Post Type');
-    $page['post_content'] = __('This is a custom post type for Learn Plugin.');
+    $page['post_title'] = __('Learn Plugin Page');
+    $page['post_content'] = __('This is a custom page for Learn Plugin.');
     $page['post_status'] = 'publish';
     $page['post_slug'] = 'learn-plugin-page';
     $page['post_type'] = 'page';
@@ -148,18 +147,25 @@ function learn_plugin_create_custom_post_type()
     $page['post_category'] = array(0);
 
     // Insert the post into the database
-    wp_insert_post($page);
+    $page_id = wp_insert_post($page); // this will create a page with the title "Learn Plugin Post Type" and content "This is a custom post type for Learn Plugin." when the plugin is activated.
 
-    // register_post_type('learn_plugin', array(
-    //     'labels' => array(
-    //         'name' => __('Learn Plugins'),
-    //         'singular_name' => __('Learn Plugin')
-    //     ),
-    //     'public' => true,
-    //     'has_archive' => true,
-    //     'supports' => array('title', 'editor', 'thumbnail')
-    // ));
+    add_option('learn_plugin_page_id', $page_id); // this will save the page id in the database, so we can use it later to delete the page when the plugin is deactivated.
+
 }
 
-// add_action('init', 'learn_plugin_create_custom_post_type');
-register_activation_hook(__FILE__, 'learn_plugin_create_custom_post_type');
+register_activation_hook(__FILE__, 'learn_plugin_create_custom_page');
+
+
+
+// delete the page created by the plugin when the plugin is deactivated
+function learn_plugin_delete_custom_page()
+{
+    $page_id = get_option('learn_plugin_page_id'); // get the page id from the database
+    if ($page_id) {
+        wp_delete_post($page_id, true); // delete the page permanently
+        delete_option('learn_plugin_page_id'); // delete the option from the database
+    }
+}
+
+register_deactivation_hook(__FILE__, 'learn_plugin_delete_custom_page'); // delete the custom page on deactivation
+// register_uninstall_hook(__FILE__, 'learn_plugin_delete_custom_page'); // delete the custom page on uninstallation
