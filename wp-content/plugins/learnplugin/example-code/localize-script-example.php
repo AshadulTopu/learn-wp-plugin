e029c57531a52f0377db36250af5b99f2dab941c
Ashadul Islam <ashadulislam2003@gmail.com>
2026-03-09 00:21:40 +0600

    learn to enqueue localize script

--- wp-content/plugins/learnplugin/example-code/localize-script-example.php
+++ wp-content/plugins/learnplugin/example-code/localize-script-example.php
@@ -1,28 +1,5 @@
 <?php
 
-// Enqueue plugin styles & scripts
-function learn_plugin_enqueue_styles()
-{
-    wp_enqueue_style('learn-plugin-main', LEARN_PLUGIN_URL . 'assets/css/style.css', array(), LEARN_PLUGIN_VERSION);
-
-}
-add_action('init', 'learn_plugin_enqueue_styles');
-
-// Enqueue plugin scripts
-function learn_plugin_enqueue_scripts()
-{
-    wp_enqueue_script('learn-plugin-main', LEARN_PLUGIN_URL . 'assets/js/main.js', array('jquery'), LEARN_PLUGIN_VERSION, true);
-}
-add_action('init', 'learn_plugin_enqueue_scripts');
-
-// // Enqueue plugin scripts
-// function learn_plugin_enqueue_admin_scripts()
-// {
-//     wp_enqueue_script('learn-plugin-admin', LEARN_PLUGIN_URL . 'assets/js/admin.js', array('jquery'), LEARN_PLUGIN_VERSION, true);
-// }
-// add_action('admin_enqueue_scripts', 'learn_plugin_enqueue_admin_scripts');
-
-
 
 // ============================================================ //
 // localize script
