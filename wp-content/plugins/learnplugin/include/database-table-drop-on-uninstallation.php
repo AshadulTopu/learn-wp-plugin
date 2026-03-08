9b7bf01ff5c0dd1ce0a981786f97cb34d0b1f6ef
Ashadul Islam <ashadulislam2003@gmail.com>
2026-03-08 19:06:32 +0600

    learn to drop database table while plugin uninstallation

---
+++ wp-content/plugins/learnplugin/include/database-table-drop-on-uninstallation.php
@@ -0,0 +1,13 @@
+<?php
+
+// uninstallation hook
+
+function learn_plugin_uninstall()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'learn_plugin_table';
+    $sql = "DROP TABLE IF EXISTS $table_name;";
+    $wpdb->query($sql);
+}
+
+register_uninstall_hook(__FILE__, 'learn_plugin_uninstall');
